e60987cb2d0c930dd8826c2b669e4cae0c8fc056
shwethasp <shwethasp05@gmail.com>
2016-12-01 16:13:32 +0530

    Initial commit

---
+++ app/src/main/java/google/vmokshagroup/com/analytics_google/WebsiteModel.java
@@ -0,0 +1,54 @@
+package google.vmokshagroup.com.analytics_google;
+
+/**
+ * Created by shwethap on 04-11-2016.
+ */
+public class WebsiteModel {
+
+    String webSiteName;
+    String webSiteType;
+   // String timeZone;
+    String webSiteId;
+
+    public String getTrackingid() {
+        return Trackingid;
+    }
+
+    public void setTrackingid(String trackingid) {
+        Trackingid = trackingid;
+    }
+
+    String Trackingid;
+
+    public String getWebSiteName() {
+        return webSiteName;
+    }
+
+    public void setWebSiteName(String webSiteName) {
+        this.webSiteName = webSiteName;
+    }
+
+    public String getWebSiteType() {
+        return webSiteType;
+    }
+
+    public void setWebSiteType(String webSiteType) {
+        this.webSiteType = webSiteType;
+    }
+
+   /* public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }*/
+
+    public String getWebSiteId() {
+        return webSiteId;
+    }
+
+    public void setWebSiteId(String webSiteId) {
+        this.webSiteId = webSiteId;
+    }
+}
